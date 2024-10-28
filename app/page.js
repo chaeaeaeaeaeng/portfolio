7d54b061945560a9349a73e7a8fc6e0770c25517
채연 <qkrcodus0403@gmail.com>
2024-10-28 15:46:43 +0900

    comittt

--- app/page.js
+++ app/page.js
@@ -1,15 +1,22 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-      <div style={{position: "relative", width: "100%", height: "100vh"}}>
-          <Image
-              src="/images/start.jpg"
-              alt="표지"
-              layout="fill"           // 이미지가 부모 요소를 채우도록 설정
-              // objectFit="cover"       // 이미지가 부모 요소에 맞게 크기 조정
-              quality={100}           // 이미지 품질 (옵션)
-          />
-      </div>
-  );
+    return (
+        <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+            <Image
+                src="/images/start.jpg"
+                alt="표지"
+                layout="fill"           // 부모 요소를 채우도록 설정
+                objectFit="cover"       // 부모 요소 비율에 맞춰 크기 조정
+                quality={100}           // 이미지 품질 (옵션)
+            />
+            <Image
+                src="/images/introduce.jpg"
+                alt="소개"
+                layout="fill"           // 부모 요소를 채우도록 설정
+                objectFit="cover"       // 부모 요소 비율에 맞춰 크기 조정
+                quality={100}           // 이미지 품질 (옵션)
+            />
+        </div>
+    );
 }
