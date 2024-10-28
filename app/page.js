416b4e786cbdfa55afc2aa495cee411c93bc3743
채연 <qkrcodus0403@gmail.com>
2024-10-28 15:40:30 +0900

    comittt

--- app/page.js
+++ app/page.js
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <img src="/images/start.jpg" alt="표지"/>
-    </div>
+      <div style={{position: "relative", width: "100%", height: "100vh"}}>
+          <Image
+              src="/images/start.jpg"
+              alt="표지"
+              layout="fill"           // 이미지가 부모 요소를 채우도록 설정
+              objectFit="cover"       // 이미지가 부모 요소에 맞게 크기 조정
+              quality={100}           // 이미지 품질 (옵션)
+          />
+      </div>
   );
 }
