ec9ab076a9a946cd68f9f5e46966d3d9e1eda54b
채연 <qkrcodus0403@gmail.com>
2024-10-28 15:42:00 +0900

    comittt

--- app/page.js
+++ app/page.js
@@ -6,8 +6,6 @@ export default function Home() {
           <Image
               src="/images/start.jpg"
               alt="표지"
-              layout="fill"           // 이미지가 부모 요소를 채우도록 설정
-              objectFit="cover"       // 이미지가 부모 요소에 맞게 크기 조정
               quality={100}           // 이미지 품질 (옵션)
           />
       </div>
